92b1a995e00ffc30f98c8d02928b0958c18cfedb
Metagesa <83432329+Metagesa@users.noreply.github.com>
2024-06-02 15:00:32 -0300

    Comentarios en index.html y main.js para mejor lectura

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ document.querySelectorAll('.faq-item').forEach(item => {
         document.querySelectorAll('.faq-respuesta').forEach(respuesta => {
             const iconn = respuesta.parentNode.querySelector('i');
 
+            // si el elemento no es el que se clickeó, se resetea a valores predeterminados
             if (respuesta != answer) {
                 respuesta.classList.remove("visible");
                 iconn.classList.remove("fa-caret-up");
@@ -13,7 +14,11 @@ document.querySelectorAll('.faq-item').forEach(item => {
             }
         });
 
+        // cambiar la visibilidad de la respuesta del elemento clickeado
+
         answer.classList.toggle("visible");
+
+        // si la respuesta se hizo visible con el toggle, se saca el icono que apunta abajo y se agrega el que apunta hacia arriba, sino se hace lo contrario
 
         if (answer.classList.contains("visible")) {
             icono.classList.remove("fa-caret-down");
