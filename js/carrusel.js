e90057b6a4eefbeaacbfa4e6e2bfa77890ed1cc1
Metagesa <83432329+Metagesa@users.noreply.github.com>
2024-06-02 14:34:26 -0300

    Cambio de carrusel owl a tiny slider en index.html
    Clase owl-carousel removida, reemplazada por tns-slider en styles.css

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,49 +1,31 @@
-$(document).ready(function () {
-    var carruselSobreMi = $('#carrusel-sobreMi');
-    carruselSobreMi.owlCarousel({
-        items: 3,
-        loop: true,
-        checkVisible: false,
-        margin: 10,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplaySpeed: 6000,
-        autoplayHoverPause: false,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 3,
-            }
-        }
-    });
+document.addEventListener('DOMContentLoaded', function () {
+    var sliders = [
+        '#carrusel-sobreMi',
+        '#carrusel-trabajos'
+    ];
 
-    var carruselTrabajos = $('#carrusel-trabajos');
-    carruselTrabajos.owlCarousel({
-        items: 3,
-        loop: true,
-        checkVisible: false,
-        margin: 10,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplaySpeed: 6000,
-        autoplayHoverPause: false,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1,
-            },
-            600: {
-                items: 2,
-            },
-            1000: {
-                items: 5,
+    sliders.forEach(function (slider) {
+        tns({
+            container: slider,
+            items: 3,
+            slideBy: 1,
+            autoplay: true,
+            autoplayButtonOutput: false,
+            autoplayTimeout: 4000,
+            speed: 3000,
+            controls: false,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1,
+                },
+                600: {
+                    items: 2,
+                },
+                1000: {
+                    items: 3,
+                }
             }
-        }
+        });
     });
 });
