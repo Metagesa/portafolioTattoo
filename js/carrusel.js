77e9bb28ee235c13d26a7409daba9bcb8c124fe0
Metagesa <83432329+Metagesa@users.noreply.github.com>
2024-06-02 22:36:58 -0300

    Correcciones de tamaños

--- js/carrusel.js
+++ js/carrusel.js
@@ -3,22 +3,33 @@ document.addEventListener('DOMContentLoaded', function () {
         container: '#carrusel-sobreMi',
         items: 1,
         slideBy: 1,
-        center: true,
-        gutter: 10,
         autoplay: false,
         autoplayButtonOutput: false,
         controls: false,
-        nav: false
+        gutter: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 3,
+            }
+        }
     });
+
     var sliderTrabajos = tns({
         container: '#carrusel-trabajos',
         items: 3,
         slideBy: 1,
-        gutter: 19,
         autoplay: true,
         autoplayButtonOutput: false,
         autoplayTimeout: 4000,
         speed: 3000,
+        gutter: 10,
         controls: false,
         nav: false,
         responsive: {
