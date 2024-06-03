ff595285d33a325eeab8be202a01df63a43bd54e
Metagesa <83432329+Metagesa@users.noreply.github.com>
2024-06-02 21:50:27 -0300

    Formulario ahora envia correos

--- js/validacion.js
+++ js/validacion.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('contactForm');
 
     form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
         // Remover clases de errores previos
         const errorElements = document.querySelectorAll('.error');
         errorElements.forEach(element => {
@@ -14,29 +16,49 @@ document.addEventListener('DOMContentLoaded', function () {
         if (!emailPattern.test(email)) {
             alert('Por favor, ingresa un correo electrónico válido.');
             document.getElementById('email').classList.add('error');
-            event.preventDefault();
             return;
         }
 
-        // Validación del campo de imagen (opcional)
+        // Validación del campo de imagen
         const fileInput = document.getElementById('referencia');
         const file = fileInput.files[0];
         if (file && file.size > 3 * 1024 * 1024) { // 3MB
             alert('La imagen no debe pesar más de 3MB.');
             document.getElementById('referencia').classList.add('error');
-            event.preventDefault();
             return;
         }
 
         // Validación de campos obligatorios
         const camposObligatorios = document.querySelectorAll('.obligatorio');
+        let allFieldsValid = true;
         camposObligatorios.forEach(campo => {
             const valorCampo = campo.value.trim();
             if (!valorCampo) {
                 alert('Por favor, completa todos los campos obligatorios.');
                 campo.classList.add('error');
-                event.preventDefault();
+                allFieldsValid = false;
             }
         });
+
+        if (!allFieldsValid) {
+            return;
+        }
+
+        const formData = new FormData(form);
+
+        // Datos a enviar en el correo. Las imagenes no se envian porque el servicio de emailJS no lo permite, es funcion de pago.
+        const emailParams = {
+            nombre: formData.get('nombre'),
+            email: formData.get('email'),
+            mensaje: formData.get('mensaje'),
+        };
+
+        emailjs.send('service_zfol05y', 'template_dof3w2b', emailParams)
+            .then(function(response) {
+                alert('Email enviado con éxito!');
+            }, function(error) {
+                console.error('Error al enviar el email:', error);
+                alert('Fallo al enviar el email. Por favor, inténtelo de nuevo más tarde.');
+            });
     });
 });
